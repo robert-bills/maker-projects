a9b6ea07d53843036de73df27c08c64c1fdc937c
Robert Bills <robert.al.bills@gmail.com>
2026-09-04 21:24:49 -0500

    adding table creation scripts

---
+++ python-database/creat_sensors_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "sensors" (
+	"sensor"	TEXT UNIQUE,
+	"sensorId"	NUMERIC NOT NULL UNIQUE,
+	PRIMARY KEY("sensorId")
+)
---
+++ python-database/create_weather_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "weather_data" (
+	"reading_id"	INTEGER,
+	"reading_date"	TEXT,
+	"sensor"	TEXT,
+	"temperature"	INTEGER,
+	"humidity"	INTEGER,
+	"pressure"	INTEGER
+)
