16dba22a2383479614db710bb447fdc6d345f21c
Robert Bills <robert.al.bills@gmail.com>
2026-09-04 19:21:41 -0500

    adding the stub python file

---
+++ python-database/process.py
@@ -0,0 +1,17 @@
+# process.py  
+# grab some data, do a fake normalization, make a database
+
+
+# open a CSV file
+
+# load the data
+
+# separate a repeating qualifier out
+
+# create a new database file (not worried about servers right now)
+
+# create a table to hold the repeating data
+# load the data
+
+# create a table to hold referenced data 
+# load the data
